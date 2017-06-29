31fcf934ec4ac98975c807848f95f2c8469dc1c8
zjsxdyw <zjsxdyw@gmail.com>
2017-06-29 17:10:09 +0800

    Update router.js

--- Router/router.js
+++ Router/router.js
@@ -31,6 +31,15 @@
                 curMap = pathMap;
             });
         },
+        remove: function (path) {
+            var curMap = pathMap,
+                paths = removeSlash(path).replace(/^\//, '').split('/');
+            for (var i = 0, len = paths.length; i < len; i++) {
+                if (len - 1 === i) delete curMap[path];
+                else if (!curMap[path]) break;
+                curMap = curMap[path].children;
+            }
+        },
         pushHash: function (path) {
             window.location.hash = path;
         },
