023d0322663f8d18602830377371aec5adbf8736
zjsxdyw <zjsxdyw@gmail.com>
2017-08-08 17:35:05 +0800

    Update code.js

--- Ajax Queue/code.js
+++ Ajax Queue/code.js
@@ -1,3 +1,40 @@
+// Plan A
+var ajaxForQueue = (function($) {
+    var queue = [],
+        _ajax = $.ajax;
+
+    function next(done) {
+        if(done) {
+            queue.shift();
+        }
+        if(queue.length) {
+            queue[0].resolve();
+        }
+    }
+
+    return function() {
+        var deferred = $.Deferred(),
+            promise = $.Deferred(),
+            args = [].slice.call(arguments);
+
+        queue.push(promise);
+
+        promise.then(function() {
+            return _ajax.apply($, args);
+        }).then(function() {
+            deferred.resolve.apply(deferred, [].slice.call(arguments));
+            next(true);
+        }).fail(function() {
+            deferred.reject.apply(deferred, [].slice.call(arguments));
+            next(true);
+        });
+
+        next();
+
+        return deferred;
+    }
+})($);
+// Plan B
 var ajaxForQueue = (function($) {
     var queue = [],
         idle = true,
