721e88325585cdc38c655dc8498be9960619dc7d
zjsxdyw <zjsxdyw@gmail.com>
2018-11-30 16:32:07 +0800

    Update index.js

--- login/index.js
+++ login/index.js
@@ -1,5 +1,5 @@
 // plan A
-var createRequest = (send, check, login) => {
+const createRequest = (send, check, login) => {
   let arr = [];
   let isLogin = false;
 
@@ -47,85 +47,94 @@ const hookAjax = require("ajax-hook");
 const map = {};
 const realxhr = XMLHttpRequest;
 let uid = 0;
-let login = true;
 
-const check = function(result) {
-  return login;
-};
-
-const handler = function(xhr, event) {
-  if(xhr.status >= 200 && xhr.status < 300) {
-    let result;
-    try {
-      result = JSON.parse(xhr.response);
-    } catch(e) {
-      result = xhr.response;
+const done = () => {
+  for(let key in map) {
+    let xhr = new realxhr;
+    for(let obj of map[key]) {
+      if(obj.method) {
+        xhr[obj.method].apply(xhr, obj.arg);
+      } else if(obj.event) {
+        xhr[obj.event] = function() {
+          obj.fn.apply(obj.xhr, arguments);
+        }
+      }
     }
-    if(check(result)) {
+  }
+}
+
+
+const interceptor = (check, login) => {
+  const handler = (xhr, event) => {
+    if(check(xhr)) {
       map[xhr.uid].push({
         event,
         fn: xhr[event],
         xhr
       });
+      login(done);
       return true;
     }
+    delete map[xhr.uid];
   }
-  delete map[xhr.uid];
-}
-
-hookAjax({
-  //hook callbacks
-  onreadystatechange(xhr) {
-    if(xhr.readyState === 4) {
-      return handler(xhr, 'onreadystatechange');
-    }
-  },
-  onload(xhr) {
-    return handler(xhr, 'onload');
-  },
-  onerror(xhr) {
-    delete map[xhr.uid];
-  },
-  onabort(xhr) {
-    delete map[xhr.uid];
-  },
-  ontimeout() {
-    delete map[xhr.uid];
-  },
-  //hook function
-  open(arg, xhr) {
-    this.uid = uid++;
-    map[this.uid] = [];
-    map[this.uid].push({
-      method: 'open',
-      arg
-    });
-  },
-  send(arg, xhr) {
-    map[this.uid].push({
-      method: 'send',
-      arg
-    });
-  },
-  setRequestHeader(arg, xhr) {
-    map[this.uid].push({
-      method: 'setRequestHeader',
-      arg
-    });
-  }
-});
-
-// login
-login = false;
-for(let key in map) {
-  var xhr = new realxhr;
-  for(let obj of map[key]) {
-    if(obj.method) {
-      xhr[obj.method].apply(xhr, obj.arg);
-    } else if(obj.event) {
-      xhr[obj.event] = function() {
-        obj.fn.apply(obj.xhr, arguments);
+  
+  hookAjax({
+    //hook callbacks
+    onreadystatechange(xhr) {
+      if(xhr.readyState === 4) {
+        return handler(xhr, 'onreadystatechange');
       }
+    },
+    onload(xhr) {
+      return handler(xhr, 'onload');
+    },
+    onerror(xhr) {
+      delete map[xhr.uid];
+    },
+    onabort(xhr) {
+      delete map[xhr.uid];
+    },
+    ontimeout() {
+      delete map[xhr.uid];
+    },
+    //hook functions
+    open(arg, xhr) {
+      this.uid = uid++;
+      map[this.uid] = [];
+      map[this.uid].push({
+        method: 'open',
+        arg
+      });
+    },
+    send(arg, xhr) {
+      map[this.uid].push({
+        method: 'send',
+        arg
+      });
+    },
+    setRequestHeader(arg, xhr) {
+      map[this.uid].push({
+        method: 'setRequestHeader',
+        arg
+      });
     }
-  }
+  });
 }
+
+module.export = interceptor;
+
+// test
+let needLogin = true;
+
+const check = function(result) {
+  return needLogin;
+}
+
+const login = function(done) {
+  setTimeout(() => {
+    needLogin = false;
+    done();
+  }, 5000)
+}
+
+interceptor(check, login);
